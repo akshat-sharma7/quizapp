94ad68c2a2959ec3f822da4b2afcb93b31e59334
Akshat <aksharma07@yahoo.com>
2025-07-03 03:06:37 +0530

    added ResponseEntity

--- quizapp/src/main/java/com/project/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/project/quizapp/controller/QuestionController.java
@@ -4,7 +4,15 @@ import com.project.quizapp.dto.QuestionUpdateRequest;
 import com.project.quizapp.model.Question;
 import com.project.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,17 +24,17 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
         return questionService.getQuestionByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody List<Question> question){
+    public ResponseEntity<String> addQuestion(@RequestBody List<Question> question){
         return questionService.addQuestion(question);
     }
 
--- quizapp/src/main/java/com/project/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/project/quizapp/service/QuestionService.java
@@ -3,10 +3,11 @@ package com.project.quizapp.service;
 import com.project.quizapp.dao.QuestionDao;
 import com.project.quizapp.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -14,17 +15,32 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(List<Question> question) {
-        questionDao.saveAll(question);
-        return "success";
+    public ResponseEntity<String> addQuestion(List<Question> question) {
+        try {
+            questionDao.saveAll(question);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     public void deleteQuestion(Integer id) {
